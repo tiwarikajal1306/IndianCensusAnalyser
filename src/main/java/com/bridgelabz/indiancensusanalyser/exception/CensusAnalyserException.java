27ab6588bebed3fedec28ff3c4277625ce3a00f6
kajal <tiwarikajal1306@gmail.com>
2020-06-29 20:00:00 +0530

    ADDED Check to ensure number of records matches and if csv file incorrect returns exception

--- src/main/java/com/bridgelabz/indiancensusanalyser/exception/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/indiancensusanalyser/exception/CensusAnalyserException.java
@@ -1,5 +1,16 @@
 package com.bridgelabz.indiancensusanalyser.exception;
 
 public class CensusAnalyserException extends Exception {
-
+   public enum ExceptionType {
+        CENSUS_FILE_PROBLEM
+    }
+    public ExceptionType type;
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
 }
